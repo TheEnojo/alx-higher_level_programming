d8c31e83a4652cbb5c005aef7eeffc6110dd9213
Enojo Godwin <105020842+TheEnojo@users.noreply.github.com>
2022-08-24 03:46:38 +0100

    Updated

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+for alphabets in range(ord('a'), ord('z')+1):
+
+    if alphabets in [101, 113]:
+
+        continue
+
+    print("{:c}".format(alphabets), end="")
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def islower(c):
+
+    for s in range(97, 123):
+
+        if (ord(c) == s):
+
+            return True
+
+        else:
+
+            continue
+
+        return False
